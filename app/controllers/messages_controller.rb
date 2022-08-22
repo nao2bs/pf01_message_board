63394c0484196f5aa883de6ca7c6c21b9f5c4cfc
NAO <nao2bs@gmail.com>
2022-08-22 08:21:56 +0000

    change docker-compose.yml

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,45 +1,44 @@
 class MessagesController < ApplicationController
-  before_action :set_message, only: [:edit, :update, :destroy]
-  
+  before_action :set_message, only: %i[edit update destroy]
+
   def index
     @message = Message.new
     @messages = Message.all
   end
-  
-  def create 
+
+  def create
     @message = Message.new(message_params)
     if @message.save
-      redirect_to root_path, notice: "メッセージを保存しました。"
+      redirect_to root_path, notice: 'メッセージを保存しました。'
     else
       @messages = Message.all
-      flash.now[:alert] = "メッセージの保存に失敗しました。"
+      flash.now[:alert] = 'メッセージの保存に失敗しました。'
       render 'index'
-    end  
+    end
   end
-  
-  def edit
-  end
-  
+
+  def edit; end
+
   def update
     if @message.update(message_params)
       redirect_to root_path, notice: 'メッセージを編集しました。'
     else
       render 'edit'
-    end  
+    end
   end
-  
+
   def destroy
     @message.destroy
     redirect_to root_path, notice: 'メッセージを削除しました'
-  end  
-    
-  
+  end
+
   private
+
   def message_params
     params.require(:message).permit(:name, :body, :age)
-  end  
-  
+  end
+
   def set_message
     @message = Message.find(params[:id])
-  end  
+  end
 end
